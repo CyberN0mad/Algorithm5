732c3fb1e432ee7e6e0f915d9d9b068a9e4a6d5f
CyberN0mad <71142523+CyberN0mad@users.noreply.github.com>
2021-05-22 12:25:55 +0600

    Shaker sort

---
+++ src/com/company/Main.java
@@ -0,0 +1,46 @@
+package com.company;
+
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        int temp; // znachenie
+        int arraySize = 20;
+        int[] array = new int[arraySize];
+        Random newRandom = new Random();
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = newRandom.nextInt(20) +1;
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+
+        int leftSide = 0;
+        int rightSide = array.length - 1;
+
+        do {
+            for (int i = leftSide; i < rightSide; i++) { // <- max
+
+                if (array[i] > array[i + 1]) {
+                    temp = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = temp;
+                }
+            }
+            rightSide--; // - kolichestvo prohodov
+            for (int i = rightSide; i > leftSide; i--) { // -> min
+                if (array[i] < array[i - 1]) {
+                    temp = array[i];
+                    array[i] = array[i - 1];
+                    array[i - 1] = temp;
+                }
+            }
+            leftSide++; // - s obratnoi storony
+        } while (leftSide < rightSide); // uslovie
+        for (int i : array
+        ) {
+            System.out.print(array[i] + " ");
+        }
+    }
+    
+}
